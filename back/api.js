52894c4f24fa2fee3a1c70e5dd40b4da1ad22b5b
Zack-Strife <hugo.capezzone@gmail.com>
2017-05-23 18:32:35 +0200

    api improvements

--- back/api.js
+++ back/api.js
@@ -5,10 +5,11 @@ const mongoose = require('mongoose');
 
 const ListController = require('./controller/List');
 
+
 const bodyParser = require('body-parser');
 const router = Router();
 
-mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds129281.mlab.com:29281/heroku_db`);
+mongoose.connect(`mongodb:<dbuser>:<dbpassword>@ds129281.mlab.com:29281/heroku_db`);
 
 router.use(bodyParser.json({'extended': true}));
 router.use(bodyParser.json());
@@ -18,5 +19,14 @@ router.get('/', (req, res) => {
 });
 
 router.get('/lists', ListController.getLists);
+router.get('/list/:id', ListController.getSingleList);
+
+router.post('/lists', ListController.createList);
+// router.put('/lists/:id', ListController.updateList);
+// router.delete('/lists/:id', ListController.deleteList);
+router.post('/lists/:id/task', ListController.createTask);
+// router.put('/lists/:id/task/:taskId', ListController.updateTask);
+// router.delete('/lists/:id/task/:taskId', ListController.deleteTask);
+
 
 module.exports = router;
--- back/controller/List.js
+++ back/controller/List.js
@@ -11,7 +11,74 @@ const ListController = {
 			if (err) { res.send(err) }
 			res.json(lists);
 		})
-	}
+	},
+
+	createList: (req, res) => {
+		var list = new List();
+	    list.title = req.body.title;
+	    list.save((err) => {
+	      if (err) {
+	        return res.send(err);
+	      }
+	      res.json({
+	        message: `List ${list.title} created !`
+	      });
+	    });
+	},
+
+	// updateList: (req, res) => {
+	//     List.findByIdAndUpdate(req.params.id, { $set: req.body}, {new: true}, (err, project) => {
+	//       if (err) {
+	//         return res.send(err).status(500);
+	//       }
+	//       res.json(project)
+	//     });
+	// },
+
+	// deleteList: (req, res) => {
+	//     List.findByIdAndRemove(req.params.id, (err, project) => {
+	//       if (err) {
+	//         return res.send(err).status(500);
+	//       }
+	//       let str = (list) ? `List ${project.title} deleted !` : 'No list were deleted.'
+	//       res.json({ message: str })
+	//     });
+	// },
+	
+	getSingleList: (req, res) => {
+		List.findOne({_id: req.params.id}, function(err, doc) {
+			res.json(doc);
+		});
+	},
+
+	createTask: (req, res) => {
+		var list = {};
+		var query = List.findOne({_id: req.params.id});
+		query.exec(function (err, document) {
+		  if (err) return handleError(err);
+		  list =  document;
+		  newTask = new Object;
+		  newTask.title = req.body.title;
+		  var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+		  var uniqid = randLetter + Date.now();
+		  newTask.id = uniqid;
+		  newTask.user = req.body.user;
+		  state = new Object;
+		  state.title = req.body.state.title;
+		  state.color = req.body.state.color;
+		  newTask.state = state;
+		  list.tasks.push(newTask);
+		  list.save((err) => {
+		      if (err) {
+		        return res.send(err);
+		      }
+		      res.json({
+		        message: `Task created into list ${list.title} !`
+		      });
+		  });
+		})
+		
+	},
 }
 
 module.exports = ListController;
