4af78236fd4c1cd05d76b9dff19312174456408d
Zack-Strife <hugo.capezzone@gmail.com>
2017-05-30 11:16:30 +0200

    added push method

--- back/api.js
+++ back/api.js
@@ -9,7 +9,7 @@ const ListController = require('./controller/List');
 const bodyParser = require('body-parser');
 const router = Router();
 
-mongoose.connect(`mongodb:<dbuser>:<dbpassword>@ds129281.mlab.com:29281/heroku_db`);
+mongoose.connect(`mongodb://socketIoUser:antoine2199@ds129281.mlab.com:29281/heroku_db`);
 
 router.use(bodyParser.json({'extended': true}));
 router.use(bodyParser.json());
@@ -25,7 +25,7 @@ router.post('/lists', ListController.createList);
 // router.put('/lists/:id', ListController.updateList);
 // router.delete('/lists/:id', ListController.deleteList);
 router.post('/lists/:id/task', ListController.createTask);
-// router.put('/lists/:id/task/:taskId', ListController.updateTask);
+router.put('/lists/:id/task/:taskId', ListController.updateTask);
 // router.delete('/lists/:id/task/:taskId', ListController.deleteTask);
 
 
