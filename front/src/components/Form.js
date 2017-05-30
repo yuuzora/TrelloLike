9298dcbdf1d2a8adcc858d77f1eba167c61816ef
Axel-Charpentier <axelcharpentier0@gmail.com>
2017-05-30 11:22:41 +0200

    Edit the form section for more visibility... bullshit

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -4,28 +4,33 @@ export default class Form extends Component {
     render() {
         return (
             <div className="col-xs-6">
-                <div className="form-group">
-                    <input className="form-control" placeholder="Add a list" />
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-primary">Add a list</button>
-                </div>
-                <hr />
-                <div className="form-group">
-                    <input className="form-control" placeholder="Add a task" />
-                </div>
-                <div className="form-group">
-                    <input className="form-control" placeholder="Enter a user" />
-                </div>
-                <div className="form-group">
-                    <select className="form-control">
-                        {this.props.lists.map(list => (
-                        <option value="">{list.title}</option>
-                        ))}
-                    </select>
-                </div>
-                <div className="form-group">
-                    <button className="btn btn-primary">Add a task</button>
+                <div className="row">
+                    <div className="col-xs-6">
+                        <div className="form-group">
+                            <input className="form-control" placeholder="Add a list" />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary">Add a list</button>
+                        </div>
+                    </div>
+                    <div className="col-xs-6">
+                        <div className="form-group">
+                            <input className="form-control" placeholder="Add a task" />
+                        </div>
+                        <div className="form-group">
+                            <input className="form-control" placeholder="Enter a user" />
+                        </div>
+                        <div className="form-group">
+                            <select className="form-control">
+                                {this.props.lists.map(list => (
+                                <option value="">{list.title}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary">Add a task</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
